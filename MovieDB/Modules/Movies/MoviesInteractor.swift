b836081a364f68c25da7c1f99c4e710c0025be27
Yris Siqueira <siqueirayris@hotmail.com>
2019-12-25 02:04:49 -0300

    Fetching data

--- MovieDB/Modules/Movies/MoviesInteractor.swift
+++ MovieDB/Modules/Movies/MoviesInteractor.swift
@@ -8,14 +8,38 @@
 
 import Foundation
 
+// MARK: - Interactor Protocol
+
 protocol MoviesInteractorProtocol: class {
-    func fetchMovies()
+    func fetchMoviesFromPage(completion: @escaping (Result<[Movie], Error>) -> Void)
 }
 
-final class MoviesInteractor: MoviesInteractorProtocol {
+final class MoviesInteractor: API, MoviesInteractorProtocol {
     
-    func fetchMovies() {
-        
+    // MARK: - Attributes
+    
+    private var serviceKey: String = "16fd5a2a34d2c3651edf338a920f6176"
+    private var path: String = "https://api.themoviedb.org/3"
+    
+    
+    func fetchMoviesFromPage(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        let params = [
+           "api_key": serviceKey
+        ]
+
+        request(queryParams: params, urlPath: "\(path)/movie/top_rated") { response in
+            switch response {
+            case .failure(let error):
+                completion(.failure(error))
+            case .success(let data):
+                do {
+                    let response = try JSONDecoder().decode(MoviesResponse.self, from: data)
+                    completion(.success(response.results))
+                } catch let error {
+                    completion(.failure(error))
+                }
+            }
+        }
     }
     
 }
