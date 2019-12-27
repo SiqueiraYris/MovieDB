1a83c791763d85d681c148cb5979dc02310dfb29
Yris Siqueira <siqueirayris@hotmail.com>
2019-12-27 03:44:07 -0300

    Creating extension to round image

--- MovieDB/Extensions/UIImageView+Extensions.swift
+++ MovieDB/Extensions/UIImageView+Extensions.swift
@@ -35,3 +35,16 @@ extension UIImageView {
     }
     
 }
+
+extension UIImageView {
+
+    // MARK: - Round Image
+    func roundImage() {
+        self.layer.borderWidth = 1
+        self.layer.masksToBounds = false
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
+    }
+    
+}
